564b7047982cc134d37f44c91407d12252054979
Alaa A. Abdelhamid <130788379+A-A-Abdelhamid@users.noreply.github.com>
2025-07-15 14:13:05 -0400

    wip

--- AODSkimmer/scripts/ElectronNtuplizer_cfg.py
+++ AODSkimmer/scripts/ElectronNtuplizer_cfg.py
@@ -34,7 +34,7 @@ options.register('numThreads',
         VarParsing.VarParsing.varType.int,
         "Number of threads (for CRAB vs non-CRAB execution)")
 options.register("nEvents",
-	-1,
+	100,
 	VarParsing.VarParsing.multiplicity.singleton,
         VarParsing.VarParsing.varType.int,
 	"Number of events to process (defaults to all)")
