41cf08dbe334ea0a2fef79c147db201e35bd3978
Alaa A. Abdelhamid <130788379+A-A-Abdelhamid@users.noreply.github.com>
2025-07-16 09:59:02 -0400

    wip

--- AODSkimmer/plugins/ElectronSkimmer.cc
+++ AODSkimmer/plugins/ElectronSkimmer.cc
@@ -810,7 +810,7 @@ ElectronSkimmer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
          nt.recoDSAMuonTrkNumHits_.push_back(track.hitPattern().numberOfValidMuonHits());
          nt.recoDSAMuonTrkNumPlanes_.push_back(track.hitPattern().muonStationsWithValidHits());
 
-         float passesDisplacedId = 0;
+         int passesDisplacedId = 0;
          if(passesDisplacedID(track) passesDisplacedId=1;
          nt.recoDSAMuonDisplacedId_.push_back(passesDisplacedId);
 
