32c2e38d29284f7e59d8f8dd298e9d12d36acdef
Alaa A. Abdelhamid <130788379+A-A-Abdelhamid@users.noreply.github.com>
2026-05-07 13:40:52 -0400

    Update script to use python3 and set up CMSSW_13_0_13

--- python_analysis/condor/skimming_rdf/run_skim_rdf.sh
+++ python_analysis/condor/skimming_rdf/run_skim_rdf.sh
@@ -4,8 +4,17 @@ outDir=$2
 MET_cut=$3
 nJet_cut=$4
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_103/x86_64-centos7-gcc11-opt/setup.sh
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=el8_amd64_gcc11
+
+# Set up the CMSSW release on the worker node
+scram p CMSSW CMSSW_13_0_13
+cd CMSSW_13_0_13/src
+eval `scramv1 runtime -sh`
+cd -
+
+# Explicitly run with python3, passing your arguments
 tar xzf ${jobname}.tar.gz
-python condor_skim_rdf.py -n ${jobname} -o ${outDir} -m ${MET_cut} -j ${nJet_cut}
+python3 condor_skim_rdf.py -n ${jobname} -o ${outDir} -m ${MET_cut} -j ${nJet_cut}
 #echo "xrdcp -f *.root root://cmseos.fnal.gov/${outDir}/"
 #xrdcp -f *.root root://cmseos.fnal.gov/${outDir}/
